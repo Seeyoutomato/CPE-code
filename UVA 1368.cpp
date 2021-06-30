69e92375a94ce660bdcd8f578c5fbfe39a186b40
Seeyoutomato <41862716+Seeyoutomato@users.noreply.github.com>
2021-06-30 23:12:11 +0800

    Update UVA 1368.cpp

--- UVA 1368.cpp
+++ UVA 1368.cpp
@@ -12,7 +12,7 @@ char who_biggest(int a,int b,int c,int d){
 	else if(d>a && d>b && d>c)
 		return 'T';
 }
-char data[55][1005];
+char data[55][1005];      //這兩個要宣告為全域變數 否則容量不夠
 char ans[51];
 int main(){
 	int n,m,gh,cou,cou1,cou2,cou3; 
@@ -55,4 +55,56 @@ int main(){
 			cout << ans[u];
 		cout << endl << wrongans << endl;		
 	}
+}
+
+// 以下是別人的 三個迴圈更簡單省事
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+int nums[55];
+char dnas[55][1005];
+char const_dna[4] = {'T', 'G', 'C', 'A'};
+int main()
+{
+    int cases,i,j,k,n,m;
+    int minNum;
+    int minDnaIndex;
+    char ansDns[1005];
+    int ansSum, tempSum;
+    scanf("%d", &cases);
+    while(cases--){
+        scanf("%d %d", &m, &n);
+        for(i = 0 ; i < m;++i){
+            nums[i] = 0;
+            scanf("%s", dnas[i]);
+        }
+
+        ansSum = 0;
+        for(i = 0 ; i < n ;++i){            //關鍵
+            minNum = INT_MAX;
+            minDnaIndex = -1;
+            for(j = 0 ; j < 4;++j)         //關鍵
+            {
+                tempSum = 0;
+                for(k = 0 ; k < m;++k)     //關鍵
+                {
+                    if(const_dna[j] != dnas[k][i]){
+                        tempSum++;
+                    }
+                }
+
+                if(tempSum <= minNum){
+                    minDnaIndex = j;
+                    minNum = tempSum;
+                }
+            }
+            ansSum += minNum;
+            ansDns[i] = const_dna[minDnaIndex];
+        }
+        ansDns[n] = '\0';
+
+        printf("%s\n%d\n", ansDns, ansSum);
+    }
+    return 0;
 }
